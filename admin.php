32d608b43923cd56f9ddfbc470145353f8d79196
Prathoshini07 <prathoshini07@gmail.com>
2024-03-16 00:44:27 +0530

    added admin.php

--- admin.php
+++ admin.php
@@ -2,18 +2,21 @@
     <head>
         <title>Admin Module</title>
         <link rel="stylesheet" type="text/css" href="Styles.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
     </head>
     <body>
         <div id="section2">
-        <h1>ADMIN</h1>
-        <table cellspacing="15px" style="text-align:center">
-            <tr>
+        <h1 style="padding-top:100px">ADMIN</h1>
+        <table cellspacing="15px" cellpadding="15px" style="text-align:center">
+            <tr style="background-color: rgba(255,255,255,0.5)">
                 <td>Add Employee</td>
                 <td>Alter Employee Details</td>
                 <td>Delete Employee</td>
                 <td>Room Allotment</td>
             </tr>
-            <tr>
+            <tr style="background-color: rgba(255,255,255,0.5)">
                 <td>Get Patient Details</td>
                 <td>Salary</td>
                 <td>Get Appointment Details</td>
